80c27a9ee721ba8abe5ea360e954c4d09adbcd66
xxh18 <xxhthu18@163.com>
2020-11-01 19:08:36 +0800

    连接服务器数据库

--- C03/C03/settings.py
+++ C03/C03/settings.py
@@ -73,13 +73,25 @@ WSGI_APPLICATION = 'C03.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'platform',
+        'USER': 'debian-sys-maint',
+        'PASSWORD': '1OpN9k8rLhRFyFBC',
+        'HOST': '140.143.163.227',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
