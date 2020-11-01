9da1c173b92e0cf49ac24b61ee29f397541f09d3
Chenbaixu <chenbx18@mails.tsinghua.edu.cn>
2020-11-01 12:42:58 +0800

    吃个饭回来再冲
    Hhhhh

--- C03/app/models.py
+++ C03/app/models.py
@@ -22,6 +22,17 @@ class Manager(models.Model):
     workPlaceId = models.IntegerField()
     loginToken = models.CharField(max_length=100)
     email = models.EmailField(null=True)
-    # TODO:加入未读通知列表和违规次数
+    # TODO:加入未读通知列表
 
+
+class Stadium(models.Model):
+    # 场馆
+    name = models.CharField(max_length=32)
+    information = models.CharField(max_length=300)
+    # TODO:开放时间和关闭时间可以设置为DateField
+    openTime = models.CharField(max_length=32)
+    closeTime = models.TimeField(max_length=32)
+    contact = models.IntegerField(null=True)
+    openState = models.BooleanField()
+    foreDays = models.IntegerField()
 
