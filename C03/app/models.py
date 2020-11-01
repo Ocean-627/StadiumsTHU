6bf258b0a7716872bfbc5235bdb4fcc752fd85a1
Chenbaixu <chenbx18@mails.tsinghua.edu.cn>
2020-11-01 12:35:17 +0800

    加了个测试接口test
    返回Just for test

--- C03/app/models.py
+++ C03/app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    # 普通用户
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    userId = models.IntegerField()
+    loginToken = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    phone = models.IntegerField(null=True)
+    # TODO:加入未读通知列表和违规次数
+
+
+class Manager(models.Model):
+    # 场馆管理员
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    userId = models.IntegerField()
+    workPlace = models.CharField(max_length=32)
+    workPlaceId = models.IntegerField()
+    loginToken = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    # TODO:加入未读通知列表和违规次数
+
+
