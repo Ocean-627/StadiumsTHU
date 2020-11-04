95ad08eaf23af0cbb004f843b8deb377f80932d2
Chenbaixu <chenbx18@mails.tsinghua.edu.cn>
2020-11-04 09:54:15 +0800

    事实证明可以直接Migrate
    所以还是用服务器上的数据库吧

--- C03/C03/settings.py
+++ C03/C03/settings.py
@@ -73,14 +73,15 @@ WSGI_APPLICATION = 'C03.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+'''
 DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3',
      }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -91,7 +92,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-'''
 
 
 # Password validation
