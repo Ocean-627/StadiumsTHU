6532ed1c0380db39c3840810f076abc613edf56d
Con6924 <43441391+Con6924@users.noreply.github.com>
2020-11-16 21:21:52 +0800

    Web前端部分

---
+++ Web-Frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>清动家园管理平台</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
---
+++ Web-Frontend/src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Header></Header>
+        <div class="dropdown-divider"></div>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar"
+import Header from "../components/Header"
+export default {
+    data(){
+        return {}
+    },
+    components: {
+        Navbar, Header
+    }
+}
+</script>
