35e69cacc7d255704005e6fff1480f75fa50ede6
Chenbaixu <chenbx18@mails.tsinghua.edu.cn>
2020-12-04 15:07:52 +0800

    #24
    单元测试写不动，快进到系统测试

--- C03/app/test/usertest1.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import status
-from rest_framework.test import APITestCase
-from rest_framework.test import RequestsClient
-
-from app.models import *
-
-
-# Create your tests here.
