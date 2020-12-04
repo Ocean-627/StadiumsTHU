3abc5c095b73e7289ae8df5fa1d93e06dd0bd851
Chenbaixu <chenbx18@mails.tsinghua.edu.cn>
2020-12-04 14:42:22 +0800

    #24
    之前的测试显然是不能用了，更新一批，然而还没整明白怎么写

---
+++ C03/app/test/usertest1.py
@@ -0,0 +1,8 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import RequestsClient
+
+from app.models import *
+
+
+# Create your tests here.
