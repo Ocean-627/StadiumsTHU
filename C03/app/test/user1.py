602dcda4f1d4768f3d97d28ea8e789fab00c90d9
Chenbaixu <chenbx18@mails.tsinghua.edu.cn>
2020-12-08 15:45:36 +0800

    test
    testCase

--- C03/app/test/user1.py
+++ C03/app/test/user1.py
@@ -10,10 +10,11 @@ class GetStadiumTest(TestCase):
         User.objects.create(openId='cbx', loginToken=1)
         Stadium.objects.create(name='综合体育馆', information='综合体育馆', openTime='08:00', closeTime='18:00', openState=True,
                                foreDays=3)
+        self.headers = {'HTTP_loginToken': 1}
 
     def test_get(self):
         params = {}
-        resp = self.client.get('/api/user/stadium/', params, loginToken=1)
+        resp = self.client.get('/api/user/stadium/', params, **self.headers)
         content = json.loads(resp.content)
         print(content)
         self.assertEqual(resp.status_code, 200)
