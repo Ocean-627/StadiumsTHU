00baa51b86fad4dc6a49e886dab8d1f7816e6628
Con6924 <43441391+Con6924@users.noreply.github.com>
2020-12-24 01:52:34 +0800

    css fix

--- Web-Frontend/vue.config.js
+++ Web-Frontend/vue.config.js
@@ -1,11 +1,11 @@
- module.exports = {
-     devServer: {
-         proxy: {
-             '/': {
-                 target: 'http://127.0.0.1:8000',
-                 ws: true,
-                 changeOrigin: true
-             }
-         }
-     }
- }
+module.exports = {
+  devServer: {
+    proxy: {
+      "/": {
+        target: "http://127.0.0.1:8000",
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+};
